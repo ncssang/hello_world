1757872b451f688c29dbed245186435e49e95fa8
Nguyen Cong Sang <cs.ncsang@gmail.com>
2019-12-08 15:16:45 +0700

    Add basic_math

---
+++ src/basic_math.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    float a, b, c, d, e;
+    a = 2;
+    b = 4;
+    c = a + b;
+    d = a / b;
+    e = a * b;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "c = a + b = " << c << std::endl;
+    std::cout << "d = a / b = " << d << std::endl;
+    std::cout << "e = a * b = " << e << std::endl;
+
+    std::cout << "____________\n";
+}
